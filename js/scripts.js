eafc84d8c1247b49d4ea00bc9c2550d5d7789160
RBaltazar <rodolpho.baltazar@hotmail.com>
2022-10-06 16:13:39 -0300

    final

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar Shrink Function/Redução Navbar
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink The Navbar
+    navbarShrink();
+
+    // Navbar When Page Is Scrolled/Navbar Pagina E Rolagem
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap Scrollspy Main Nav Element/Ativação Bootstrap Scroll Pagina Principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse Responsive Navbar When Toggler Is Visible/Recolher Navbar
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
